77c703eaa25d1ab36d00c65b3a3f6f15064f761f
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-09-15 18:13:17 -0300

    Tutoriais

---
+++ app/src/main/java/com/example/aplicativopi/Menu_Tutoriais.kt
@@ -0,0 +1,11 @@
+package com.example.aplicativopi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Tutoriais : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_tutoriais)
+    }
+}
